71d8f063b64287cefd6dee93c284f56b91b6c5c6
pupil <pupil@ivkhk.ee>
2022-04-20 15:20:28 +0300

    start

---
+++ GeneratedPassword.py
@@ -0,0 +1,9 @@
+from math import random
+
+small_letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','s','t','u','v','w','x','y','z']
+large_letters = ['A','B','C','D','E','F','G','H','I', 'J','K','L','M','N','O','P','Q', 'S','T','U','V','W','X','Y','Z']
+
+max_symbols = 0
+
+while max_symbols < 15:
+    
